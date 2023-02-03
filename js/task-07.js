3d62fc801da1a92af72d97a72f04fba7ae7dc172
Taras Papka <taras.papka@protonmail.com>
2023-02-03 09:29:55 +0200

    set the input value when loaded

--- js/task-07.js
+++ js/task-07.js
@@ -15,6 +15,7 @@
 
 const input = document.querySelector('#font-size-control');
 const text = document.querySelector('#text');
+text.style.fontSize = input.value + "px";
 
 function handleRange(e) {
   text.style.fontSize = e.currentTarget.value + "px";
